21828dec57a08e645d1d12ff78d977878072d2ac
DanieleAntonaci <danieleanto96@gmail.com>
2023-01-30 16:35:35 +0100

    creato header

--- resources/views/pages/layout-pagina.blade.php
+++ resources/views/pages/layout-pagina.blade.php
@@ -1,7 +1,30 @@
-@extends('welcome')
+@extends('layout.app')
 
 @section('header')
-   <div>
-    
+
+<header>
+   <div class="my_container">
+
+      <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="logo">
+
+     <nav>
+            <ul >
+               <li><a href="#">CHARACTERS</a> </li>
+               <li><a href="#">COMICS</a> </li>
+               <li><a href="#">MOVIE</a> </li>
+               <li><a href="#">TV</a> </li>
+               <li><a href="#">GAMES</a> </li>
+               <li><a href="#">COLLECTIBLES</a> </li>
+               <li><a href="#">VIDEOS</a> </li>
+               <li><a href="#">FANS</a> </li>
+               <li><a href="#">NEWS</a> </li>
+               <li><a href="#">SHOP</a> </li>
+            </ul>
+     </nav>
+
+      <div>
+         <input type="search" name="search">
+      </div>
    </div>
-@endsection
+ </header>
+   @endsection
